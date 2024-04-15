8f4c5d5804ec2d5b6de982f8d2b27fca32f3673b
Courtesi <c.chang673@gmail.com>
2024-04-14 22:49:19 -0700

    href = index.html

---
+++ target/cs122b-project/index.js
@@ -0,0 +1,71 @@
+/**
+ * This example is following frontend and backend separation.
+ *
+ * Before this .js is loaded, the html skeleton is created.
+ *
+ * This .js performs two steps:
+ *      1. Use jQuery to talk to backend API to get the json data.
+ *      2. Populate the data to correct html elements.
+ */
+
+
+/**
+ * Handles the data returned by the API, read the jsonObject and populate data into html elements
+ * @param resultData jsonObject
+ */
+function handleMovieResult(resultData) {
+    console.log("handleMovieResult: populating movie table from resultData");
+
+    // Populate the movie table
+    // Find the empty table body by id "movie_table_body"
+    let movieTableBodyElement = jQuery("#movie_table_body");
+
+    // Iterate through resultData, no more than 10 entries
+    for (let i = 0; i < Math.min(20, resultData.length); i++) {
+
+        // Concatenate the html tags with resultData jsonObject
+        let rowHTML = "";
+        rowHTML += "<tr>";
+        rowHTML +=
+            "<th>" +
+            // Add a link to single-movie.html with id passed with GET url parameter
+            '<a href="single-movie.html?id=' + resultData[i]['movie_id'] + '">' +
+            resultData[i]["movie_title"] +     // display star_name for the link text
+            '</a>' +
+            "</th>";
+        rowHTML += "<th>" + resultData[i]["movie_year"] + "</th>";
+        rowHTML += "<th>" + resultData[i]["movie_director"] + "</th>";
+        rowHTML += "<th>" + resultData[i]["movie_genres"] + "</th>";
+
+        const list_of_stars = resultData[i]["movie_stars"].split(",");
+        const list_of_stars_ids = resultData[i]["stars_ids"].split(",");
+
+        rowHTML += "<th>"
+        for (let j = 0; j < list_of_stars.length; j++) {
+            rowHTML += '<a href="single-star.html?id=' + list_of_stars_ids[j] + '">'
+            + list_of_stars[j] + '</a>';
+            if (j !== (list_of_stars.length - 1)) {
+                rowHTML += ", "
+            }
+        }
+        rowHTML += "</th>";
+
+        rowHTML += "<th>" + resultData[i]["movie_rating"] + "</th>";
+        rowHTML += "</tr>";
+
+        // Append the row created to the table body, which will refresh the page
+        movieTableBodyElement.append(rowHTML);
+    }
+}
+
+
+/**
+ * Once this .js is loaded, following scripts will be executed by the browser
+ */
+
+jQuery.ajax({
+    dataType: "json", // Setting return data type
+    method: "GET", // Setting request method
+    url: "api/movies", // Setting request url, which is mapped by StarsServlet in Stars.java
+    success: (resultData) => handleMovieResult(resultData) // Setting callback function to handle data returned successfully by the StarsServlet
+});
